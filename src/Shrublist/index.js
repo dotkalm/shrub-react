37302778066dd4dc5afb073b31e6c0fd5d806875
dotkalm <joelmholmberg@gmail.com>
2019-08-23 10:13:49 -0700

    display shrub ai

--- src/Shrublist/index.js
+++ src/Shrublist/index.js
@@ -5,14 +5,16 @@ import {
 } from './style'
 
 const Shrubs = (props) => {
-   
+   console.log(props)
    const shrubList = props.shrubs.map((shrub,i) => {
 
       return (
+
          <span key={shrub.id} className='card'>
             
             <img src = {'http://localhost:8000/shrub_pics/' + shrub.image} alt={shrub.description}/>
-            uploaded by the {shrub.author.username}
+            {/* uploaded by the {shrub.author.username} */}
+            {shrub.detect_shrub ? 'nice shrub' : 'not sure if this is a shrub'}
          </span>
       )
    })
